97aa7aafef21f1b65b531a62c5121aaa5ba109c9
vallabh <vallabh.joshi@gmail.com>
2023-11-02 07:42:18 +0000

    added downloader script

--- download/download.sh
+++ download/download.sh
@@ -1,14 +1,79 @@
+# Set -e to exit on error
+set -e
 
+echo
+echo "📦 OCSF-Tool Downloading"
+echo
 
+# Set Constants
 FILE_BASENAME="ocsf-tool"
-RELEASES_URL="https://github.com/valllabh/ocsf-tool/releases/latest"
+RELEASES_URL="https://github.com/valllabh/ocsf-tool/releases"
 
+
+# Detect OS and Architecture
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 test "$ARCH" = "aarch64" && ARCH="arm64"
+
+echo "✅ OS and Architecture detected"
+
+
+# Tar File Name Pattern
 TAR_FILE="${FILE_BASENAME}_${OS}_${ARCH}.tar.gz"
 
-echo $TAR_FILE
+# Get the latest release URL
+
+LATEST_VERSION_URL=$(curl -sSLI -o /dev/null -w %{url_effective} "$RELEASES_URL/latest")
+
+# extract the latest release version from the URL
+VERSION=$(echo $LATEST_VERSION_URL | grep -oP '(?<=/tag/).*$')
+echo "✅ Detected latest version of OCSF-Tool"
+
+
+
+# Download the tar file and checksums
+curl -sfLO "$RELEASES_URL/download/$VERSION/$TAR_FILE"
+curl -sfLO "$RELEASES_URL/download/$VERSION/checksums.txt"
+
+echo "✅ Downloaded OCSF-Tool $VERSION (latest)"
+
+
+# Exit if Tar file is not donwloaded
+if [ ! -f "$TAR_FILE" ]; then
+    echo "❌ Could not download files."
+    echo "Please use Release URL to download the files manually."
+    echo $RELEASES_URL 
+    exit 1
+fi
+
+# Verify Checksums
+sha256sum --ignore-missing --quiet --check checksums.txt
+echo "✅ Verified downloaded files"
+
+# Installation
+
+# Create ocsf-tool directory if does not exists
+mkdir -p "./ocsf-tool"
+
+# Clear ocsf-tool directory
+rm -rf "./ocsf-tool/*"
+
+tar -xf "$TAR_FILE" -C "./ocsf-tool"
+echo "✅ Extracted the tar file in ./ocsf-tool directory"
+
+
+
+# remove tar file and checksums
+rm -rf "$TAR_FILE" "checksums.txt"
+echo "✅ Deleted tar file and checksums"
+
+
+echo "✅ Dwonload Complete!"
+echo
+
+
+./ocsf-tool/ocsf-tool --help
+
 
 # #!/bin/bash
 # set -e
