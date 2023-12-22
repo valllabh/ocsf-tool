35a0d86e2bf769c131aac08e9b51d0c191a90ea1
vallabh <vallabh.joshi@gmail.com>
2023-12-22 05:24:06 +0000

    Command schema class list can write to json file.

--- commands/schema_class_list.go
+++ commands/schema_class_list.go
@@ -1,7 +1,10 @@
 package commands
 
 import (
+	"encoding/json"
+
 	"github.com/spf13/cobra"
+	"github.com/valllabh/ocsf-tool/commons"
 	"github.com/valllabh/ocsf-tool/ocsf/schema"
 )
 
@@ -13,16 +16,41 @@ var SchemaClassListCmd = &cobra.Command{
 	Run:   runSchemaClassListCmd,
 }
 
+func init() {
+	// Add flags to the SchemaClassListCmd command
+	rootCmd.AddCommand(SchemaClassListCmd)
+
+	// Add flag to output json file path
+	SchemaClassListCmd.Flags().StringP("output", "", "", "Output JSON file path")
+
+}
+
 // Define the run function for the SchemaClassListCmd command
 func runSchemaClassListCmd(cmd *cobra.Command, args []string) {
 
 	ocsfSchema := schema.LoadOCSFSchema()
 	classes := ocsfSchema.Classes
 
+	// get output file path
+	outputFilePath := cmd.Flag("output").Value.String()
+
 	// Group classes by Category
 	classesByCategory := make(map[string][]string)
 	for _, class := range classes {
 		classesByCategory[class.Category] = append(classesByCategory[class.Category], class.Name)
+	}
+
+	if outputFilePath != "" {
+		contents, _ := json.Marshal(classesByCategory)
+		// Write classes by Category to output file
+		outputFilePathError := commons.CreateFile(outputFilePath, contents)
+
+		if outputFilePathError != nil {
+			println("Error writing to output file: " + outputFilePath)
+		} else {
+			println("Classes by Category written to output file: " + outputFilePath)
+		}
+		return
 	}
 
 	// Print classes by Category
@@ -32,9 +60,4 @@ func runSchemaClassListCmd(cmd *cobra.Command, args []string) {
 			println("    " + class)
 		}
 	}
-}
-
-func init() {
-	// Add flags to the SchemaClassListCmd command
-	rootCmd.AddCommand(SchemaClassListCmd)
 }
