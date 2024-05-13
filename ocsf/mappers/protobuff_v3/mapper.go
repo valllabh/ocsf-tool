ed29478e60b7df423c47e061632b61873ce63fc1
Paul Querna <pquerna@apache.org>
2024-05-13 14:33:09 -0700

    cleanup logic

--- ocsf/mappers/protobuff_v3/mapper.go
+++ ocsf/mappers/protobuff_v3/mapper.go
@@ -122,18 +122,16 @@ func (mapper *mapper) populateFieldsFromAttributes(message *Message, attributes 
 			field.Type = FIELD_TYPE_OBJECT
 		}
 
-		if len(attr.Enum) > 0 {
-			field.Type = FIELD_TYPE_ENUM
-			if field.DataType == "string_t" {
-				// there are a couple string enums that are not really strings
-				// and not really enums, but we treat them as string primatives
-				field.Type = FIELD_TYPE_PRIMITIVE
-				validKeys := make([]string, 0, len(attr.Enum))
-				for aek := range attr.Enum {
-					validKeys = append(validKeys, aek)
-				}
-				field.Comment["AllowedValues"] = strings.Join(validKeys, ", ")
+		if len(attr.Enum) > 0 && attr.Type == "string_t" {
+			// there are a couple string_t enums that should just be represented as strings
+			field.Type = FIELD_TYPE_PRIMITIVE
+			validKeys := make([]string, 0, len(attr.Enum))
+			for aek := range attr.Enum {
+				validKeys = append(validKeys, aek)
 			}
+			field.Comment["AllowedValues"] = strings.Join(validKeys, ", ")
+		} else if len(attr.Enum) > 0 {
+			field.Type = FIELD_TYPE_ENUM
 		}
 
 		// Processing Based on Type
